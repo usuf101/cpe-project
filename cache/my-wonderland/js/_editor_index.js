2649e47b57df5ef36977c936a79c48695a0bfe4d
usuf101 <ygm2017@nyu.edu>
2025-09-28 19:41:37 +0400

    Initial commit

---
+++ cache/my-wonderland/js/_editor_index.js
@@ -0,0 +1,4 @@
+import * as __wonderlandengine_components from '@wonderlandengine/components';
+_registerEditor(__wonderlandengine_components);
+import * as _C__Users_vrsc4_Documents_WonderlandEngine_my_wonderland_js_button_js from 'C:/Users/vrsc4/Documents/WonderlandEngine/my-wonderland/js/button.js';
+_registerEditor(_C__Users_vrsc4_Documents_WonderlandEngine_my_wonderland_js_button_js);
